fe32bd3ac9d73ee1feb456e8feda60a4f210edf2
Pepper <supergonzalez360@gmail.com>
2021-12-06 23:01:21 -0700

    Integracion de pruebas v1

---
+++ micro-servicios/test/pruebasListaSorteos.test.js
@@ -0,0 +1,67 @@
+const fetch = require('cross-fetch');
+
+
+// fetch("http://localhost:3312/api/v1/sorteos/lista").then(res=>res.json()).then(data=>{
+//    console.log(data);
+// });
+test("Obtener almenos un sorteo",(done)=>{
+  fetch("http://localhost:3312/api/v1/sorteos/lista").then(res=>res.json()).then(data=>{
+    expect(data.length).toBeGreaterThanOrEqual(1);
+    done();
+  });
+});
+
+// fetch("http://localhost:3312/api/v1/sorteos/listaVigentes").then(res=>res.json()).then(data=>{
+//  console.log(data);
+// });
+test("Que solo muestren los sorteos vigentes", (done)=>{
+  fetch("http://localhost:3312/api/v1/sorteos/listaVigentes").then(res=>res.json()).then(data=>{
+    let check = true;
+
+    for(x of data){
+      if(x.estadoSorteo!="VIGENTE")
+      check = false;
+    }
+
+    expect(check).toBe(true);
+    done();
+  });
+});
+// fetch("http://localhost:3312/api/v1/sorteos/listaEspera").then(res=>res.json()).then(data=>{
+//   console.log(data);
+// });
+test("Que solo muestren los sorteos en espera", (done)=>{
+  fetch("http://localhost:3312/api/v1/sorteos/listaEspera").then(res=>res.json()).then(data=>{
+    let check = true;
+
+    for(x of data){
+      if(x.estadoSorteo!="ESPERA")
+      check = false;
+    }
+
+    expect(check).toBe(true);
+    done();
+  });
+});
+
+// fetch("http://localhost:3312/api/v1/sorteos/listaTerminados").then(res=>res.json()).then(data=>{
+//     console.log(data);
+// });
+
+test("Que solo muestren los sorteos terminados", (done)=>{
+  fetch("http://localhost:3312/api/v1/sorteos/listaTerminados").then(res=>res.json()).then(data=>{
+    let check = true;
+
+    for(x of data){
+      if(x.estadoSorteo!="TERMINADO")
+      check = false;
+    }
+
+    expect(check).toBe(true);
+    done();
+  });
+});
+
+
+
+
---
+++ micro-servicios/test/pruebasTablero.test.js
@@ -0,0 +1,33 @@
+const fetch = require('cross-fetch');
+
+
+
+// fetch("http://localhost:3312/api/v1/sorteos/lista/618c71b24fffe30e88c60c6f").then(res=>res.json()).then(data=>{
+// console.log(data);
+// });
+
+test("Que solo muestre la informacion de tablero de un sorteo", (done) => {
+    fetch("http://localhost:3312/api/v1/sorteos/lista/618c71b24fffe30e88c60c6f").then(res => res.json())
+        .then(data => {
+            expect(data.sorteo).toBe("Cuatrimoto");
+            done();
+        });
+});
+
+
+
+// fetch("http://localhost:3312/api/v1/reporte/numeros/todos/618c71b24fffe30e88c60c6f").then(res=>res.json()).then(data=>{
+//     console.log(data);
+// });
+
+// fetch("http://localhost:3312/api/v1/reporte/numeros/apartados/618c71b24fffe30e88c60c6f").then(res=>res.json()).then(data=>{
+//    console.log(data);
+// });
+
+// fetch("http://localhost:3312/api/v1/reporte/numeros/pagados/618c71b24fffe30e88c60c6f").then(res=>res.json()).then(data=>{
+//   console.log(data);
+// });
+
+// fetch("http://localhost:3312/api/v1/reporte/numeros/disponibles/618c71b24fffe30e88c60c6f").then(res=>res.json()).then(data=>{
+//    console.log(data);
+// });
